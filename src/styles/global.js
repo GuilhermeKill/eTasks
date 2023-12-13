d5951fcdf38447a6d288fdbfd79a24a29a6f033c
Guilhermekill <guilherme.reiskill2002@gmail.com>
2023-12-13 15:36:14 -0300

    first commit

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,10 +1,19 @@
 import { createGlobalStyle } from "styled-components";
+import { DEVICE_BREAKPOINTS } from "./deviceBreakpoints";
+
 
 export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+  :root{
+    font-size: 16px;
+
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}){
+      font-size: 12px;
+    }
   }
 
   body {
@@ -34,7 +43,7 @@ export default createGlobalStyle`
   }
 
   ::-webkit-scrollbar {
-    width: 8px;
+    width: 0.5rem;
   }
   
   ::-webkit-scrollbar-track {
